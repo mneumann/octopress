7cb50304bc0441ee4967bc5fae756c024981e5ce
Michael Neumann <mneumann@ntecs.de>
2014-02-23 15:16:20 +0100

    Configure

--- config.rb
+++ config.rb
@@ -4,11 +4,11 @@ require 'sass-globbing'
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
+http_path = "/mn/"
+http_images_path = "/mn/images"
 http_generated_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_fonts_path = "/mn/fonts"
+css_dir = "public/mn/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
